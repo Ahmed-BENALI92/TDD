3dc6a7124228e764d80eb427514f05d4a57cb532
Ahmed BENALI <37007849@parisnanterre.fr>
2021-09-16 15:22:35 +0200

    Add Before

--- app/src/test/java/toto1/DictionaryTest.java
+++ app/src/test/java/toto1/DictionaryTest.java
@@ -1,14 +1,20 @@
 package toto1;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import static org.hamcrest.MatcherAssert.assertThat; 
 import static org.hamcrest.Matchers.*;
 
 public class  DictionaryTest {
-
+    public Dictionary dict;
+    
+    @Before public void initiation()
+    {
+        Dictionary dict = new Dictionary("Ex");
+    }    
     @Test  public void testDictionaryName() 
         {
-        Dictionary dict = new Dictionary("Ex");
+        
         assertThat(dict.getName(), equalTo("Example"));
         }
     
