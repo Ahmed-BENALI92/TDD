d1703210c5c6ecc40600c6116d776c74fec66f02
Ahmed BENALI <37007849@parisnanterre.fr>
2021-09-16 14:36:54 +0200

    Create Constructor and Getname

--- app/src/main/java/toto1/Dictionary.java
+++ app/src/main/java/toto1/Dictionary.java
@@ -2,6 +2,16 @@ package toto1;
 
 public class Dictionary
 {
+    public String Name;
+    
+    public  Dictionary()
+    {
 
+    }
+
+    public String getName()
+    {
+        return Name;
+    }
 
 }
