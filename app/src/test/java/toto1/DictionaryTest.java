e3b82e5bb79082c24f531c98fdcbe910072bb97d
Ahmed BENALI <37007849@parisnanterre.fr>
2021-09-16 23:24:27 +0200

    Try test jacoco

--- app/src/test/java/toto1/DictionaryTest.java
+++ app/src/test/java/toto1/DictionaryTest.java
@@ -1,10 +1,17 @@
 package toto1;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 import static org.junit.Assert.*;
 import org.junit.Before;
 import static org.hamcrest.MatcherAssert.assertThat; 
 import static org.hamcrest.Matchers.*;
 
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+ DictionaryTest.class,
+})
 public class  DictionaryTest {
     public Dictionary dict;
 
